0f55c4a233ec8c2f6ce705df88dd84b65b738f11
Tvorg <gsignat.ua@gmail.com>
2023-07-14 23:39:06 +0300

    new magasin
    new magasin

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-function initMap() {
+function initMap1() {
     let pos = {lat: 50.467338, lng: 30.471332};
     let map = new google.maps.Map(document.getElementById("map"), {
         center: pos,
@@ -9,4 +9,17 @@ function initMap() {
         map: map,
         title: "AutoVegas"
     });
+}
+function initMap2(){
+    let pos = {lat: 50.422395, lng: 30.378578};
+    let map = new google.maps.Map(document.getElementById("map"), {
+        center: pos,
+        zoom: 17
+    });
+    let marker = new google.maps.Marker({
+        position: pos,
+        map: map,
+        title: "AutoVegas"
+    });
+
 }
